6cdc83877ba8049427605b0b9fc2ddaf44f209d8
Kevin Haube <khaube@fearless.tech>
2023-02-04 13:43:33 -0500

    cleanup

--- src/components/input/SongInput.vue
+++ src/components/input/SongInput.vue
@@ -4,16 +4,15 @@
     <button>Submit</button>
   </div>
 </template>
-  
+
 <styles lang="css">
 #url-entry-field {
-    @apply flex pt-4
+  @apply flex pt-4
 }
 input {
-    @apply bg-slate-100 hover:bg-white rounded-sm text-sm py-1 px-3
+  @apply bg-slate-100 hover:bg-white rounded-sm text-sm py-1 px-3
 }
 button {
-    @apply font-bold text-sm max-h-10 px-4 rounded-r-sm bg-blue-500 text-white
+  @apply font-bold text-sm max-h-10 px-4 rounded-r-sm bg-blue-500 text-white
 }
 </styles>
-  
